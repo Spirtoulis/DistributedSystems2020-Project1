4603159e1d9031d26596e42ffef7116eacf693b0
Spirtoulis <teotselikis@gmail.com>
2020-04-08 14:26:30 +0300

    Delete Broker2.java

--- src/Broker2.java
+++
@@ -1,8 +0,0 @@
-public class Broker2 {
-
-    public static void main(String[] args){
-        Broker broker2 = new Broker("Broker2");
-        broker2.init(1481);
-        broker2.connect();
-    }
-}
