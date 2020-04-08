b77a62c435ffc459e492116bc0c8963760a2811d
Spirtoulis <teotselikis@gmail.com>
2020-04-08 14:27:03 +0300

    Delete Broker4.java

--- src/Broker4.java
+++
@@ -1,8 +0,0 @@
-public class Broker4 {
-
-    public static void main(String[] args){
-        Broker broker4 = new Broker("Broker4");
-        broker4.init(1666);
-        broker4.connect();
-    }
-}
